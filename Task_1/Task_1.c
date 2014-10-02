9da53e7182bc839e0bf71653e9d9d5e8dc536f5a
KrowosDogg <djrpg7@gmail.com>
2014-10-02 22:22:08 +0400

    Fixed out. Added enum. Added solve.c. Fixed solve.h.

---
+++ Task_1/Task_1.c
@@ -0,0 +1,11 @@
+#include "solve.h"
+int main()
+{
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
+    int root = TWO_ROOTS;
+    in (&a, &b, &c);
+    square (a, b, c, &x1, &x2, &root);
+    out (&x1, &x2, &root);
+    system ("PAUSE");
+    return 0;
+}
---
+++ Task_1/solve.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+#include "solve.c"
+void in (double *a, double *b, double *c);
+void out (double *x1, double *x2, int *root);
+void square (double a, double b, double c, double *x1, double *x2, int *root);
