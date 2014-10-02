f8985acb215cd19b50217c2e5805f4f21817e2e5
KrowosDogg <djrpg7@gmail.com>
2014-10-02 22:21:31 +0400

    Fixed out. Added enum. Added solve.c. Fixed solve.h.

--- Task_1/solve.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include "solve.c"
-void in (double *a, double *b, double *c);
-void out (double *x1, double *x2, int *root);
-void square (double a, double b, double c, double *x1, double *x2, int *root);
